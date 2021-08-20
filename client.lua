d777eaf113e27521e5aaeb8008fddeedf490d6a5
Pushkar Tatiya <pushkart2@gmail.com>
2021-08-20 12:13:21 -0500

    fixed client

--- client.lua
+++ client.lua
@@ -1,3 +1,4 @@
+local isLoggedIn = false
 local inRange = false
 local prop_model = {
 	["Standard CPU"] = "v_corp_servercln",
@@ -16,25 +17,50 @@ Citizen.CreateThread(function()
 		if #(pos - Config.shop["coords"]) < 5.0 then
 			inRange = true
 			DrawText3Ds(Config.shop["coords"], "~g~E~w~ - Open Shop")
-			if IsControlJustPressed(0, 38)
-				TriggerServerEvent("inventory:server:OpenInventory", "shop", "Itemshop_DigitalDen", Config.ShopItems)
+			if IsControlJustPressed(0, 38) then
+				TriggerServerEvent("inventory:server:OpenInventory", "shop", "TestItemshop_DigitalDen", Config.ShopItems)
 			end
 		end
 
 		if active_machines ~= nil then
 			for k, v in pairs(active_machines) do
 				if #(pos - v.coords) < 3.0 then
+					inRange = true
 					DrawText3Ds(v.coords, "Mining Time Left: ~r~"..v.time)
-					DrawText3Ds(v.coords[1], v.coords[2], v.coords[3]+0.3, "Machine: ~b~"..v.name)
+					DrawText3Ds(vector3(v.coords.x, v.coords.y, v.coords.z+0.3), "Machine: ~b~"..v.name)
 				end
 			end
 		end
 
+		if #(pos - Config.MiningLab["coords"]) < 3.0 then
+			inRange = true
+			DrawText3Ds(Config.MiningLab["coords"], "~g~E~w~ - Exit")
+			if IsControlJustPressed(0, 38) then
+				TeleportToLocation("out")
+			end
+		end
+
+		if #(pos - vector3(Config.ExitCoords.x,Config.ExitCoords.y,Config.ExitCoords.z)) < 3.0 then
+			inRange = true
+			DrawText3Ds(Config.ExitCoords, "~g~E~w~ - Enter Mining Lab")
+			if IsControlJustPressed(0, 38) then
+				TeleportToLocation("in")
+			end
+		end
+
 		if not inRange then
-			Cititzen.Wait(1000)
+			Citizen.Wait(1000)
 		end
 	end
 end)
+
+function TeleportToLocation(location)
+	if location == "out" then
+		SetEntityCoords(PlayerPedId(), Config.ExitCoords)
+	elseif location == "in" then
+		SetEntityCoords(PlayerPedId(), Config.MiningLab["coords"])
+	end
+end
 
 Citizen.CreateThread(function()				
 	while true do
@@ -44,6 +70,7 @@ Citizen.CreateThread(function()
 					v.time = v.time - 1 
 				else
 					TriggerServerEvent("qb-cryptomining:server:addCryptoCoins", v.reward)
+					DeleteObject(v.object)
 					table.remove(active_machines, k)
 				end
 			end
@@ -53,32 +80,54 @@ Citizen.CreateThread(function()
 end)
 
 RegisterNetEvent("qb-cryptomining:client:installCPU")
-AddEventHandler("qb-cryptomining:client:installCPU", function(name, reward)
+AddEventHandler("qb-cryptomining:client:installCPU", function(name, reward, item)
 	local ped = PlayerPedId()
 	local pos = GetEntityCoords(ped)
-
 	if #(pos - Config.MiningLab["coords"]) < 50.0 then
-		InstallCPU(name, reward)
+		exports["memorygame"]:thermiteminigame(10, 3, 3, 10,
+		function() -- success
+			InstallCPU(name, reward, item.name)
+		end,
+		function() -- failure
+			QBCore.Functions.Notify("You can do better than this", "error")
+   	 	end)
 	else
-		QBCore.Functions.Notify('Not a suitable for Installing','error')
+		QBCore.Functions.Notify('Not a suitable location for installing','error')
 	end
 end)
 
-function InstallCPU(name, reward)
-	Citizen.CreateThread(function()
+function InstallCPU(name, reward, itemname)
+	if #active_machines == 0 then
 		local ped = PlayerPedId()
 		local coords = GetOffsetFromEntityInWorldCoords(ped, 0.0, 2.0, 0.0)
 		local machine = {}
-		machine.object = CreateObject(GetHashKey(prop_model[name]),coords,true,true,false)
-		machine.name = name
-		machine.reward = reward
-		machine.time = Config.MiningLab["mining_time"]
-		machine.coords = coords
-		table.insert(machine, active_machines)
-		PlaceObjectOnGroundProperly(machine.object)
-        FreezeEntityPosition(machine.object, true)
-		QBCore.Functions.Notify('Installation Successfull','success')
-	end)
+		QBCore.Functions.Progressbar("cpuinstall", "Installing RIG", 10000, false, true, {
+			disableMovement = false,
+			disableCarMovement = false,
+			disableMouse = false,
+			disableCombat = true,
+		}, {
+			animDict = "random@train_tracks",
+			anim = "idle_e",
+			flags = 0,
+		}, {}, {}, function() -- Done
+			machine.object = CreateObject(GetHashKey(prop_model[name]),coords,true,true,false)
+			machine.name = name
+			machine.reward = reward
+			machine.time = Config.MiningLab["mining_time"][name]
+			machine.coords = coords
+			table.insert(active_machines, machine)
+			PlaceObjectOnGroundProperly(machine.object)
+			FreezeEntityPosition(machine.object, true)
+			QBCore.Functions.Notify('Installation Successfull','success')
+			TriggerServerEvent("qb-cryptomining:server:RemoveItem", itemname)
+		end, function() -- Cancel
+			ClearPedTasks(ped)
+			QBCore.Functions.Notify("Installation stopped..", "error")
+		end)
+	else
+		QBCore.Functions.Notify("You already have a machine running", "error")
+	end
 end
 
 function DrawText3Ds(coords, text)
