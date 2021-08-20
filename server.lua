234de3d09d938d5ce8fc46c6a3886dea3e055a4c
Pushkar Tatiya <pushkart2@gmail.com>
2021-08-20 10:56:41 -0500

    create server

---
+++ server.lua
@@ -0,0 +1,26 @@
+RegisterServerEvent('qb-cryptomining:server:addCryptoCoins')
+AddEventHandler('qb-cryptomining:server:addCryptoCoins', function(reward)
+    local src = source
+    local Player = QBCore.Functions.GetPlayer(source)
+
+    if Player.Functions.AddMoney('crypto', reward) then
+        TriggerClientEvent('QBCore:Notify', src, "You have mined "..reward.." crypto coins", 'success')
+    else
+        TriggerClientEvent('QBCore:Notify', src, "Mining Failed", 'error')
+    end
+end)
+
+QBCore.Functions.CreateUseableItem("standard_cpu", function(source, item)
+	local Player = QBCore.Functions.GetPlayer(source)
+    TriggerClientEvent("qb-cryptomining:client:installCPU", source, "Standard CPU", Config.Reward["standard_cpu"])
+end)
+
+QBCore.Functions.CreateUseableItem("e2_cpu", function(source, item)
+	local Player = QBCore.Functions.GetPlayer(source)
+    TriggerClientEvent("qb-cryptomining:client:installCPU", source, "E2 CPU", Config.Reward["e2_cpu"])
+end)
+
+QBCore.Functions.CreateUseableItem("quantum_cpu", function(source, item)
+	local Player = QBCore.Functions.GetPlayer(source)
+    TriggerClientEvent("qb-cryptomining:client:installCPU", source, "Quantum CPU", Config.Reward["quantum_cpu"])
+end)
